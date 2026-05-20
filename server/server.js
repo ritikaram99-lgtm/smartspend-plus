e6894ad24450b18030c9d857c15dad27998ad7af
ritikaram99-lgtm <ritikaram99@gmail.com>
2026-05-20 22:33:48 +0530

    fix render backend crash

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production" || process.env.RENDER) {
 
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
